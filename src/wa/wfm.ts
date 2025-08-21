1347d14c5f236b9d383206746fe96cc2c1f372d5
HeshamMoawad <heshammoawad120120@yahoo.com>
2025-08-21 12:41:22 +0300

    🚑

--- src/wa/wfm.ts
+++ src/wa/wfm.ts
@@ -1,16 +1,20 @@
 import { Chat } from 'whatsapp-web.js';
 
-export const WFM_URL = 'http://wfm.m.local/api/users/whatsapp-number';
+export const WFM_URL = 'http://192.168.11.235:8000/api/users/whatsapp-number';
 
 export const pushWFM = (account: string, user: string, chats: Chat[]) => {
   console.log('Pushing to WFM', account, user, chats.length);
-  fetch(WFM_URL, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ account, user, chats }),
-  })
-    .then(async (res) => console.log(await res.text()))
-    .catch((err) => console.log(err));
+  try {
+    fetch(WFM_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ account, user, chats }),
+    })
+      .then(async (res) => console.log(await res.text()))
+      .catch((err) => console.log("Error pushing to WFM",err));
+  } catch (error) {
+    console.log("Error pushing to WFM",error);
+  }
 };
